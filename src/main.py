4a7e34429d9fe77b269fcc81ba3425c591858da9
schef <zvanstefan@gmail.com>
2021-06-23 14:30:30 +0200

    added blink

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
+from machine import Pin
+from time import sleep
+import common
 import logic
 
+led = common.create_output(25)
 
 if __name__ == "__main__":
     print("Boot sequence start")
+    led.on()
     logic.init()
+    sleep(1)
+    led.off()
     print("Boot sequence end")
     while True:
         logic.loop()
